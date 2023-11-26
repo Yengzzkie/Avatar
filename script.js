347e1b14da8431cb00688efd909e778e601b30eb
Yengzzkie <gatchalian.manuel@gmail.com>
2023-11-26 16:29:53 -0500

    - made graphical improvements including animations and UI - added game end functionality

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let container = document.querySelector("#container");
 let score = document.querySelector(".score");
-let h1 = document.querySelector("h1");
+let gameOver = document.querySelector(".game-over");
 let winner = document.querySelector(".winner");
 let playerElement = document.querySelector(".playerElement");
 let aangElement = document.querySelector(".aangElement");
-
+let gameEnded = false;
 let playerLifePercentage = 100;
 let computerLifePercentage = 100;
+
+function endGame() {
+  gameEnded = true;
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+}
 
 const fire = document.querySelector("#fire");
 
@@ -22,9 +30,10 @@ fire.addEventListener("click", () => {
       setTimeout(() => {
         playerElement.textContent = "";
         aangElement.textContent = "";
-        h1.textContent = `Game Over!`;
+        gameOver.textContent = `Game Over!`;
         winner.textContent = getWinner();
-      }, 1000);
+        endGame();
+      }, 2000);
     }
   }
 });
@@ -41,11 +50,13 @@ water.addEventListener("click", () => {
     winner.textContent = playRound(playerChoice, computerChoice);
     if (playerLifePercentage === 0 || computerLifePercentage === 0) {
       setTimeout(() => {
+        aangContainer.classList.add(".computer-evaporate");
         playerElement.textContent = "";
         aangElement.textContent = "";
-        h1.textContent = `Game Over!`;
+        gameOver.textContent = `Game Over!`;
         winner.textContent = getWinner();
-      }, 1000);
+        endGame();
+      }, 2000);
     }
   }
 });
@@ -64,9 +75,10 @@ earth.addEventListener("click", () => {
       setTimeout(() => {
         playerElement.textContent = "";
         aangElement.textContent = "";
-        h1.textContent = `Game Over!`;
+        gameOver.textContent = `Game Over!`;
         winner.textContent = getWinner();
-      }, 1000);
+        endGame();
+      }, 2000);
     }
   }
 });
@@ -85,9 +97,10 @@ wind.addEventListener("click", () => {
       setTimeout(() => {
         playerElement.textContent = "";
         aangElement.textContent = "";
-        h1.textContent = `Game Over!`;
+        gameOver.textContent = `Game Over!`;
         winner.textContent = getWinner();
-      }, 1000);
+        endGame();
+      }, 2000);
     }
   }
 });
@@ -136,8 +149,9 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  const damage = Math.floor(Math.random() * 21)
-  //major game logic, decides who wins per round
+  const damage = Math.floor(Math.random() * 21);
+
+  // game logic, decides who wins per round
   if (
     (playerSelection === "fire" && computerSelection === "earth") ||
     (playerSelection === "earth" && computerSelection === "wind") ||
@@ -146,11 +160,26 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "water" &&
       (computerSelection === "fire" || computerSelection === "earth"))
   ) {
+    computerLifePercentage -= damage;
     if (computerLifePercentage < 0) {
       computerLifePercentage = 0; //ensure computer life points doesn't go below 0 that may cause bug on playRound
     }
-    computerLifePercentage -= damage;
+
+    compDamageNumber.textContent = `-${damage}`; //animates the damage on enemy healthbar
+
+    compDamageNumber.classList.add("animate-number");
+
+    setTimeout(() => {
+      compDamageNumber.classList.remove("animate-number");
+      compDamageNumber.textContent = "";
+    }, 1000);
+
     updateLifeBars();
+
+    if (damage >= 15) {
+      return `Critical damage! You inflicted ${damage} damage points to Aang.`;
+    }
+
     return `You Win! ${playerSelection} beats ${computerSelection}. You inflicted ${damage} damage points to Aang.`;
   } else if (
     (playerSelection === "fire" &&
@@ -160,11 +189,27 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "wind" && computerSelection === "earth") ||
     (playerSelection === "water" && computerSelection === "wind")
   ) {
-    if (playerLifePercentage < 0) { //ensures player life points doesn't go below 0 that may cause bug on playRound
+    playerLifePercentage -= damage;
+    if (playerLifePercentage < 0) {
+      //ensures player life points doesn't go below 0 that may cause bug on playRound
       playerLifePercentage = 0;
     }
-    playerLifePercentage -= damage;
+
+    damageNumber.textContent = `-${damage}`; //animates the damage on player healthbar
+
+    damageNumber.classList.add("animate-number");
+
+    setTimeout(() => {
+      damageNumber.classList.remove("animate-number");
+      damageNumber.textContent = "";
+    }, 1000);
+
     updateLifeBars();
+
+    if (damage >= 15) {
+      return `Critical damage! Aang inflicted ${damage} damage points to you.`;
+    }
+
     return `You Lose! ${computerSelection} beats ${playerSelection}. Aang inflicted ${damage} damage points to you.`;
   } else {
     return "No damage done";
@@ -183,27 +228,35 @@ function getWinner() {
 }
 
 // Get references to the life bars and their inner life elements
-let playerLife = document.querySelector(".player-life");
-let computerLife = document.querySelector(".computer-life");
+let playerLife = document.querySelector(".health-bar");
+let computerLife = document.querySelector(".comp-health-bar");
+let playerLifeDamage = document.querySelector(".health-bar-red");
+let playerLifeText = document.querySelector(".health-bar-text");
+let damageNumber = document.querySelector(".damage-number");
+let computerLifeDamage = document.querySelector(".comp-health-bar-red");
+let compLifeText = document.querySelector(".comp-health-bar-text");
+let compDamageNumber = document.querySelector(".comp-damage-number");
 
 // Function to update the life bars based on player and computer scores
 function updateLifeBars() {
   // Update the width of the life bars based on the calculated percentages
   playerLife.style.width = `${playerLifePercentage}%`;
   computerLife.style.width = `${computerLifePercentage}%`;
-  console.log(playerLifePercentage);
-  console.log(computerLifePercentage);
+  playerLifeDamage.style.width = `${playerLifePercentage}%`;
+  computerLifeDamage.style.width = `${computerLifePercentage}%`;
+  playerLifeText.textContent = `${playerLifePercentage}%`;
+  compLifeText.textContent = `${computerLifePercentage}%`;
 
-  if (playerLifePercentage <= 50) {
+  if (playerLifePercentage > 50) {
     //changes the life bars to red when it reaches 50%
-    playerLife.style.backgroundColor = "red";
-  } else {
     playerLife.style.backgroundColor = "green";
+  } else {
+    playerLife.style.backgroundColor = "rgb(255, 187, 0)";
   }
 
-  if (computerLifePercentage <= 50) {
-    computerLife.style.backgroundColor = "red";
-  } else {
+  if (computerLifePercentage > 50) {
     computerLife.style.backgroundColor = "green";
+  } else {
+    computerLife.style.backgroundColor = "rgb(255, 187, 0)";
   }
 }
